91692a5c3ad379df3b9e50310d851c45d518ca27
George Malinovski <MalinovskiGeorge@gmail.com>
2017-04-28 13:09:56 +0200

    28.04.17

--- 1.JavaSyntax/src/we.java
+++ 1.JavaSyntax/src/we.java
@@ -1,19 +1,51 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
 
 public class we {
-    public static void main(String[] args) throws Exception { //напишите тут ваш код
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int a = 0;
-        double b = 0;
-        double k = 0;
-        while (a != -1) {
-            a = Integer.parseInt(br.readLine());
-            b += a;
-            k += 1;
+    public static void main(String[] args) throws IOException, JsonMappingException {
+        //создание объекта для сериализации в JSON
+        Cat cat = new Cat();
+        cat.name = "Murka";
+        cat.age = 5;
+        cat.weight = 4;
+
+        //писать результат сериализации будем во Writer(StringWriter)
+        StringWriter writer = new StringWriter();
+
+        //это объект Jackson, который выполняет сериализацию
+        ObjectMapper mapper = new ObjectMapper();
+
+        // сама сериализация: 1-куда, 2-что
+        mapper.writeValue(writer, cat);
+
+        //преобразовываем все записанное во StringWriter в строку
+        String result = writer.toString();
+        System.out.println(result);
+
+        String jsonString = "{ \"name\":\"Murka\", \"age\":5, \"weight\":4}";
+        StringReader reader = new StringReader(jsonString);
+
+        ObjectMapper mapper2 = new ObjectMapper();
+
+        Cat cat2 = mapper2.readValue(reader, Cat.class);
+        System.out.println(cat2.name);
+    }
+
+    /**
+     *
+     */
+    @JsonAutoDetect
+    public static class Cat {
+        public String name;
+        public int age;
+        public int weight;
+
+        Cat() {
         }
-        b = b + 1;
-        k = b / (k - 1);
-        System.out.println(k);
     }
 }
