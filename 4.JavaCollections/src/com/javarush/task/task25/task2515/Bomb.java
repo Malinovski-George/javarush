4659d8d15103b6a7c94eadb293bddf642ded0ff8
George Malinovski <MalinovskiGeorge@gmail.com>
2017-04-30 21:09:20 +0200

    30.04.17

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Bomb.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Bomb.java
@@ -1,18 +1,26 @@
 package com.javarush.task.task25.task2515;
 
 /**
- * Created by Gia on 30.04.2017.
+ * Класс для бомбы.
  */
 public class Bomb extends BaseObject {
     public Bomb(double x, double y) {
         super(x, y, 1);
     }
 
-    public void move() {
-        setY(getY() + 1);
-    }
-
+    /**
+     * Отрисовываем себя на холсте.
+     */
+    @Override
     public void draw(Canvas canvas) {
         canvas.setPoint(x, y, 'B');
+    }
+
+    /**
+     * Двигаем себя вниз на один ход.
+     */
+    @Override
+    public void move() {
+        y++;
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
@@ -3,9 +3,21 @@ package com.javarush.task.task25.task2515;
 /**
  * Created by Gia on 30.04.2017.
  */
-public class SpaceShip extends BaseObject{
+public class SpaceShip extends BaseObject {
 
-    public SpaceShip(double x, double y, double radius) {
-        super(x, y, radius);
+   private double dx ;
+
+    public SpaceShip(double x, double y) {
+        super(x, y, 3);
+        dx = 0;
     }
+
+    public void moveLeft() {
+        dx = -1;
+    }
+
+    public void moveRight() {
+        dx = 1;
+    }
+
 }
