35e353ef2ab166bc023eb216b4578f30263058e9
George Malinovski <MalinovskiGeorge@gmail.com>
2017-05-02 11:58:09 +0200

    02.05.17

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
@@ -5,7 +5,7 @@ package com.javarush.task.task25.task2515;
  */
 public class SpaceShip extends BaseObject {
 
-   private double dx ;
+    private double dx;
 
     public SpaceShip(double x, double y) {
         super(x, y, 3);
@@ -20,4 +20,17 @@ public class SpaceShip extends BaseObject {
         dx = 1;
     }
 
+    public void move() {
+        x = x + dx;
+        checkBorders(radius, Space.game.getWidth() - radius + 1, 1, Space.game.getHeight() + 1);
+    }
+
+    public void draw(Canvas canvas) {
+
+    }
+
+    public void fire() {
+        Space.game.getRockets().add(new Rocket(x - 2, y));
+        Space.game.getRockets().add(new Rocket(x + 2, y));
+    }
 }
