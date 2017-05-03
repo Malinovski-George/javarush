93d5770926046bac6e696e8a6f0c373ddc2f92e3
George Malinovski <MalinovskiGeorge@gmail.com>
2017-05-03 11:14:37 +0200

    03.05.17

---
+++ 4.JavaCollections/src/com/javarush/task/task34/task3403/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task34.task3403;
+
+/* 
+Разложение на множители с помощью рекурсии
+*/
+public class Solution {
+    public void recursion(final int n) {
+        final int l = (int) Math.sqrt(n);
+        for (int i = 2; i <= l; i++) {
+            if (n % i == 0) {
+                System.out.print(i + " ");
+                recursion(n / i);
+                break;
+            }
+            if (i == l) {
+                System.out.print(n);
+            }
+        }
+    }
+}
