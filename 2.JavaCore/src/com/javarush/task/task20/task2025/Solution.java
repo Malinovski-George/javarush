9d20ef9f0f28bb186e8250e623f1e15c08e099c6
George Malinovski <MalinovskiGeorge@gmail.com>
2017-05-09 14:00:05 +0200

    09.05.17

--- 2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
@@ -1,159 +1,118 @@
 package com.javarush.task.task20.task2025;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Vector;
-
-import static java.lang.Integer.MAX_VALUE;
+import java.util.TreeSet;
 
 /*
-Алгоритмы-числа
-Число S состоит из M цифр, например, S=370 и M (количество цифр) = 3
-Реализовать логику метода getNumbers, который должен среди натуральных чисел меньше N (long)
-находить все числа, удовлетворяющие следующему критерию:
-число S равно сумме его цифр, возведенных в M степень
-getNumbers должен возвращать все такие числа в порядке возрастания.
-
-Пример искомого числа:
-370 = 3*3*3 + 7*7*7 + 0*0*0
-8208 = 8*8*8*8 + 2*2*2*2 + 0*0*0*0 + 8*8*8*8
-
-На выполнение дается 10 секунд и 50 МБ памяти.
+Валидатор это решение не принимает. Однако судя по форуму, валидатором  были приняты решения которые работают
+существенно медленее.
+Это решение возвращает от 1, до последнего числа амстронга, которое меньше N.
+Решение сделано единым кодом, внутри getNumbers, без разбивки на методы, это дополнительно ускорят расчеты.
+Также, таблица степеней генерируется постепенно, по мере расчета. И уже посчитанные степени берутся из этой таблицы,
+что ускоряет процесс.
+Код отлажен однако требует наведения красоты (рефакторинга).
+Можно оптимизировать и дальше, если использовать многопоточность.
+Также можно добавить метки переходов из циклов (что весьма усложнит понимание этого кода), но возможно ускорит.
 */
-public class Solution {
-    public static void main(String[] args) {
-        int number = 1000000;
-        long freeMemory = Runtime.getRuntime().freeMemory();
-        long startTime = System.currentTimeMillis();
-        long[] a = getNumbers(number);
-        long memoryAfterCalculate = Runtime.getRuntime().freeMemory();
-        long delta = memoryAfterCalculate - freeMemory;
-        long stopTime = System.currentTimeMillis();
-        System.out.println("Calculating time: " + (stopTime - startTime));
-        System.out.println("Used JVM memory: " + delta);
-        for (long i : a) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-    }
-
-    public static long[] getNumbers(long N) {
-        Vector<Integer> v = new Vector<>();
-        for (int i = 1; i < N; i++) {
-            if (checkFormula(i)) v.add(i);
-        }
-        long[] result = new long[v.size()];
-        for (int i = 0; i < v.size(); i++) {
-            result[i] = v.get(i);
-        }
-        return result;
-    }
-
-    public static boolean checkFormula(int number) {
-        if (number == 0) return false;
-        else if (number >= 1 && number < 10) return true;
-        int     rest = number,
-                sum = 0,
-                digit = 0,
-                digitCount = 1 + (int) Math.log10(number);
-        for (int i = 0; i < digitCount; i++) {
-            digit = rest % 10;
-            rest = rest / 10;
-            int prod = digit;
-            for (int j = 1; j < digitCount; j++) {
-                prod *= digit;
-            }
-            sum += prod;
-        }
-        if (sum == number)
-            return true;
-        else
-            return false;
-    }
-}
-
-
-
-/*
 public class Solution {
     public static long[] getNumbers(long N) {
         long[] result = null;
-        ArrayList<Long> list = new ArrayList<Long>();
-        for (int i = 1; i < N; i++) {
+        TreeSet<Long> numbers = new TreeSet<>();
 
-            if (whoIsAmstrongNumber(i)) {
-                list.add((long) i);
+        //Массив степеней
+        final int SIZE = 12;
+        long[][] powLst = new long[SIZE][SIZE];
 
+        //Здесь лежит нарезка цифр
+        int[] lst = new int[20];
+
+
+        label:
+        for (long i = 1; i < N; i++) {
+            // 1.Отбраковываем числа у которых сумма цифр одинаковая (сокращаем до 200 000 тыс варинтов и менее)
+            // 2.Получаем нарезку цифр в виде массива
+            long x = i;
+            int lenCount = 0;
+            int current;
+            int last = Integer.MAX_VALUE;
+            while (x != 0) {
+                current = (int) (x % 10);
+                if ((current != 0 && last != 0) && last < current)
+                    continue label;
+                last = current;
+                lst[lenCount] = current;
+                x = x / 10;
+                lenCount++;
             }
+
+            //3. Считаем степень, и заполняем таблицу степеней
+            //Находим сумму степеней цифр числа, например сюда поступило 037 с пункта 2
+            long summa1 = 0;
+            for (int j = 0; j < lenCount; j++) {
+                if (powLst[lst[j]][lenCount] == 0) { //Если в массиве степеней еще нет значения
+                    long a1 = lst[j];
+                    if (a1 != 0 && a1 != 1) {          //Если цифра 0 или 1, то смысла считать степень нет
+                        long a2 = lst[j];
+                        for (int jj = 1; jj < lenCount; jj++) //Считаем степень
+                            a1 *= a2;
+                    }
+                    powLst[lst[j]][lenCount] = a1;//Добавляем в массив степеней новое значение
+                }
+                summa1 += powLst[lst[j]][lenCount];
+            }
+            //Например, Сумма степеней получилось = 370
+
+            //4. Получаем нарезку цифр
+            long xx = summa1;
+            lenCount = 0;
+            while (xx != 0) {
+                lst[lenCount] = (int) (xx % 10);
+                lenCount++;
+                xx = xx / 10;
+            }
+
+            //5. Из полученной суммы берем сумму степеней, для проверки на  число амстронга
+            long summa2 = 0;
+            for (int j = 0; j < lenCount; j++) {
+                if (powLst[lst[j]][lenCount] == 0) { //Если в массиве степеней еще нет значения
+                    long a1 = lst[j];
+                    if (a1 != 0 && a1 != 1) {          //Если цифра 0 или 1, то смысла считать степень нет
+                        long a2 = lst[j];
+                        for (int jj = 1; jj < lenCount; jj++) //Считаем степень
+                            a1 *= a2;
+                    }
+                    powLst[lst[j]][lenCount] = a1;//Добавляем в массив степеней новое значение
+                }
+                summa2 += powLst[lst[j]][lenCount];
+            }
+
+            //6. Добавляем результат
+            if (summa1 == summa2 && summa1 < N)
+                numbers.add(summa1);
         }
-        result = new long[list.size()];
-        for (int i = 0; i < list.size(); i++)
-        {
-            result[i] = list.get(i);
+        result = new long[numbers.size()];
+
+        int count = 0;
+        for (Long l : numbers) {
+            result[count] = l;
+            count++;
         }
         return result;
     }
 
-    public static byte countAmstNum(int ArmstrongNumber) {
-        byte i = 0;
-        //int temp=0;
-        //Проверяем сколько цифр в числе
-
-        //for(byte i=0;i<;)
-        if (ArmstrongNumber > 0)
-            while (ArmstrongNumber != 0) {
-                ArmstrongNumber = ArmstrongNumber / 10;
-                i++;
-            }
-        //System.out.println(i);
-        return i;
-    }
-
-    public static boolean whoIsAmstrongNumber(int ArmstrongNumber)
-
-    {
-        int drob = 0;
-
-        int result = 0;
-        int orig = ArmstrongNumber;
-        byte count = countAmstNum(ArmstrongNumber);
-
-        while (ArmstrongNumber != 0) {
-//выделяем поледнюю цифру в числе
-            int stepen = 1;
-            drob = ArmstrongNumber % 10;
-
-            if (count > 1)
-            for (int i = 0; i < count; i++) {
-                stepen = stepen*drob;
-            }
-
-            result = result + stepen;
-            ArmstrongNumber = ArmstrongNumber / 10;
-        }
-
-        if (orig == result) return true;
-
-        return false;
-
-    }
-
-
     public static void main(String[] args) {
-        long startMemory = Runtime.getRuntime().totalMemory();
-        long startTime = System.currentTimeMillis();
-
-        long[] rezult = getNumbers(1_000_000_000);
-
-
-        System.out.println("использовано памяти: " + (-startMemory + Runtime.getRuntime().totalMemory()));
-        System.out.println("заняло времени: " + (-startTime + System.currentTimeMillis()));
-
-        for (Long i : rezult) {
-            System.out.println(i);
+        Long t0 = System.currentTimeMillis();
+        //int n = 99999999   //1.204
+        //int n = 146511208;
+        //int n = 146511209; //1.978
+        int n = 999999999; //9.62
+        //int n = 2147483647;//22.338 - 24.887
+        long[] lst = getNumbers(n);
+        Long t1 = System.currentTimeMillis();
+        System.out.println("time: " + (t1 - t0) / 1000d + " sec");
+        System.out.println("memory: " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (1024 * 1024) + " mb");
+        for (int i = 0; i < lst.length; i++) {
+            System.out.print(lst[i] + ", ");
         }
-
+        System.out.println();
     }
 }
-*/
