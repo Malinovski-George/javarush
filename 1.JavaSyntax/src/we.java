319e70838be084654724b7e270c50b2b6339c677
George Malinovski <MalinovskiGeorge@gmail.com>
2017-05-12 16:01:57 +0200

    12.05.17

--- 1.JavaSyntax/src/we.java
+++ 1.JavaSyntax/src/we.java
@@ -2,20 +2,14 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.IOException;
-import java.io.StringReader;
-import java.io.StringWriter;
+import java.io.*;
 
 public class we {
 
 
     class B { }
     public static void main(String[] args) {
-/*will fail - compilation error, you need an instance of Test to instantiate A*/
-      // we a = new we();
-/*will compile successfully, no instance of Test is needed to instantiate B */
-        B b = new B();
-    }
+
 
 
 
