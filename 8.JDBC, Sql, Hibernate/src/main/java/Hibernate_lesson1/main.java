9afc7fc4f34094b4f16d745fa9dca99f708135f6
George Malinovski <MalinovskiGeorge@gmail.com>
2017-05-14 09:39:59 +0200

    14.05.17

--- 8.JDBC, Sql, Hibernate/src/main/java/Hibernate_lesson1/main.java
+++ 8.JDBC, Sql, Hibernate/src/main/java/Hibernate_lesson1/main.java
@@ -16,13 +16,42 @@ public class main {
         ses.beginTransaction();
 
         Employee jim = new Employee("jim");
+        Employee tom = new Employee("tom");
+        Employee ron = new Employee("ron");
         Department managers = new Department("managers");
+        Department designers = new Department("designers");
+
         managers.getEmployies().add(jim);
+        managers.getEmployies().add(tom);
+        managers.getEmployies().add(ron);
+
         jim.setDepartment(managers);
+        tom.setDepartment(managers);
+        ron.setDepartment(managers);
 
         ses.saveOrUpdate(managers);
+        ses.saveOrUpdate(designers);
         ses.saveOrUpdate(jim);
+        ses.saveOrUpdate(tom);
+        ses.saveOrUpdate(ron);
 
+        ses.getTransaction().commit();
+
+        System.out.println("------------------------------------------");
+        // ---------------------------
+        ses.beginTransaction();
+
+        Department managers_2 = (Department) ses.load(Department.class, 1);
+        Department designers_2 = (Department) ses.load(Department.class, 2);
+        Employee[] employies = managers_2.getEmployies().toArray(
+                new Employee[]{});
+
+        for (Employee employy : employies) {
+            managers_2.getEmployies().remove(employy);
+            designers_2.getEmployies().add(employy);
+            employy.setDepartment(designers_2);
+        }
+        ses.delete(managers_2);
         ses.getTransaction().commit();
     }
 }
