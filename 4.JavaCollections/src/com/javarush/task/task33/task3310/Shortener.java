59de9be7e749b33c46616b9e00f48e4c75e37ac2
George Malinovski <MalinovskiGeorge@gmail.com>
2017-05-16 22:15:41 +0200

    16.05.17

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task33.task3310;
+
+/**
+ * Created by Gia on 16.05.2017.
+ */
+
+
+
+
+public class Shortener {
+
+    public Long getId(String string){
+        return null;
+    }
+            public  String getString(Long id){
+        return null;
+    }
+
+}
